d971ad21c96a8d6359b78e58270fb821015a1f26
Osiris-Team <59899645+Osiris-Team@users.noreply.github.com>
2023-09-04 11:41:58 +0000

    replace Canvas with Omega in ./config/locales

--- config/locales/ms.rb
+++ config/locales/ms.rb
@@ -3,13 +3,13 @@
 #
 # Copyright (C) 2023 - present Instructure, Inc.
 #
-# This file is part of Canvas.
+# This file is part of Omega.
 #
-# Canvas is free software: you can redistribute it and/or modify it under
+# Omega is free software: you can redistribute it and/or modify it under
 # the terms of the GNU Affero General Public License as published by the Free
 # Software Foundation, version 3 of the License.
 #
-# Canvas is distributed in the hope that it will be useful, but WITHOUT ANY
+# Omega is distributed in the hope that it will be useful, but WITHOUT ANY
 # WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
 # A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
 # details.
